e1fdb787d73197ef613e648e73731bddb076cd5b
Tim Griffith <grifmang@gmail.com>
2021-01-23 23:23:55 -0500

    DB edit

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    connection: 'process.env.DATABASE_URL',
     migrations: {
       directory: './data/migrations'
     },
