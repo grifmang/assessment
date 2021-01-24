00babb6c32015263d30f1b83b2991186b87db33e
Tim Griffith <grifmang@gmail.com>
2021-01-23 22:55:54 -0500

    port edit for deploy

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./api/server.js');
-const port = 5000
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
     console.log(`\n** Server is listening on port ${port} **\n`)
